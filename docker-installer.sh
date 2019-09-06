007eeb164ffae0ea8b7bb45d00faaf8800e2b43c
Vioao <vioao91@gmail.com>
2019-09-06 12:13:09 +0800

    set docker to auto start

--- docker-installer.sh
+++ docker-installer.sh
@@ -5,6 +5,7 @@ yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce docker-ce-cli containerd.io
 systemctl start docker
+systemctl enable docker
 
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
